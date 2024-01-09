446acd29e61ad4e064dc6d2ca0590743623b4912
kamau95 <linuskamau586@gmail.com>
2024-01-09 09:30:58 -0800

    on control flow

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,8 +5,8 @@ def uppercase(input_str):
 
     for char in input_str:
         if 97 <= ord(char) <= 122:  # Check if character is lowercase
-            output_str += chr(ord(char) - 32)  # Convert lowercase letter to uppercase using ASCII values
+            output_str += chr(ord(char) - 32)
         else:
             output_str += char  # Keep non-alphabetic characters unchanged
-    
+
     print(output_str)
