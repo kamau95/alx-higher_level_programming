b3d2ddb9baa72c182c5c407a68903f2bc8494061
kamau95 <linuskamau586@gmail.com>
2024-01-29 06:36:20 -0800

    code to code

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,6 +7,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+
     @property
     def width(self):
         """getter mthd for the private instance atribute width"""
@@ -15,13 +16,11 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """setter mthd for the private instance attribute width"""
-        if type(value) == int:
-            if value < 0:
-                raise ValueError("width must be >= 0")
-            else:
-                self.__width = value
-        else:
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        if type(value) is not int:
             raise TypeError("width must be an integer")
+        self.__width = value
 
     @property
     def height(self):
@@ -31,10 +30,8 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """setter for the private instance attribute height"""
-        if type(value) == int:
-            if value < 0:
-                raise ValueError("height must be >= 0")
-            else:
-                self.__height = value
-        else:
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        if type(value) is not int:
             raise TypeError("height must be an integer")
+        self.__height = value
