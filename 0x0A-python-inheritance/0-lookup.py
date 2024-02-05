5191d8f8e96c03ebb80e5dbca0caa170d2e46761
kamau95 <linuskamau586@gmail.com>
2024-02-05 13:31:59 -0800

    inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+function that returns all available attributes available
+"""
+
+
+def lookup(obj):
+    """all available mthds and attributes in an object"""
+    dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+ inheritance. It is a mechanism that allows you to create a hierarchy of classes that share a set of properties and methods by deriving a class from another class.
