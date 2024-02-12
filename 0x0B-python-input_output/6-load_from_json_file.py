13ba05df6ca700a14038100f9fd80486931d8437
kamau95 <linuskamau586@gmail.com>
2024-02-11 23:10:46 -0800

    output files

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,6 +7,6 @@ import json
 
 
 def load_from_json_file(filename):
-    """crate and object from a "JSON file" """
+    """create and object from a "JSON file" """
     with open(filename, 'r', encoding='utf-8') as f:
         return json.load(f)
