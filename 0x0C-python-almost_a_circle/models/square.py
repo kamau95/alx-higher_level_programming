4564a09949c892bbaf60a09928cd8c401f626932
kamau95 <linuskamau586@gmail.com>
2024-02-20 00:04:05 -0800

    almost a circle

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+square.py - this module will hold a class that belongs to a square
+
+author - linus
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """represents a square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        self.size = size
+        self.x = x
+        self.y = y
+        self.id = id
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Return the print() and str() representation of a Square."""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
