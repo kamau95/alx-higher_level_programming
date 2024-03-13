52c727ea440106fdb1815bc58f4a98cea8a531f3
kamau95 <linuskamau586@gmail.com>
2024-03-13 08:50:12 -0700

    sql simple cmds

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,2 @@
+--  full description of the table first_table from the database hbtn_0c_first_table;
+SHOW CREATE TABLE `first_table`;
---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,2 @@
+-- list all rows of the table in our db.
+SELECT * FROM `first_table`;
