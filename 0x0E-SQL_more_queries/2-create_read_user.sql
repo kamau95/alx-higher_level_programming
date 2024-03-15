6708c6ad4ade58584e2d9a89fe7d06a1e520065c
kamau95 <linuskamau586@gmail.com>
2024-03-15 09:47:02 -0700

    privileges

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- creates a database and creates user and assign privileges
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'locahost'
+CREATE USER IF NOT EXISTS user_0d_2@locahost
 IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
