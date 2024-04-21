4ac50721595e7d3bf9157adbb58f9f9527f0afc6
kamau95 <linuskamau586@gmail.com>
2024-04-22 02:51:30 +0300

    hardo

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const arg1 = parseInt(process.argv[2], 10);
-const arg2 =parseInt(process.argv[3], 10);
-function add(a, b) {
+const arg2 = parseInt(process.argv[3], 10);
+function add (a, b) {
   console.log(a + b);
-  }
+}
 add(arg1, arg2);
