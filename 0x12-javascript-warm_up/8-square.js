141a4efe0d3fcde3661454e4b57609b8363bf90b
kamau95 <linuskamau586@gmail.com>
2024-04-22 02:44:12 +0300

    hard

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,7 +5,7 @@ if (!isNaN(size)) {
   for (let i = 1; i <= size; i++) {
     let symbol = '';
     for (let j = 1; j <= size; j++) {
-      symbol += 'x';
+      symbol += 'X';
     }
     console.log(symbol);
   }
