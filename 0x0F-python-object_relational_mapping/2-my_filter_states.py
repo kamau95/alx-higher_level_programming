c6e5cd647f12026ec4fbabf103ddb277548d85ef
kamau95 <linuskamau586@gmail.com>
2024-05-28 10:53:55 +0300

    leoh

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+script takes four arguments and and prints values
+that match one of the argument
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+    searched = sys.argv[4]
+
+    def list_values(user, passd, dbname, searched):
+        """
+        function takes in arguments and uses on of
+        them to display contents
+        """
+        db = MySQLdb.connect(
+                host='localhost',
+                passwd=password,
+                user=username,
+                db=dbname
+                )
+        cur = db.cursor()
+        query = """
+        SELECT *
+        FROM states
+        WHERE name LIKE BINARY '{}'
+        ORDER BY id ASC
+        """.format(searched)
+        cur.execute(query)
+        my_records = cur.fetchall()
+        for record in my_records:
+            print(record)
+        cur.close()
+        db.close()
+    list_values(username, password, dbname, searched)
