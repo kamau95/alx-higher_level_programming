99148e86caa4120bcc0e7823fe63b19f2e1897a1
kamau95 <linuskamau586@gmail.com>
2024-06-05 20:56:53 +0300

    semistandard

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,37 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.height = h;
+      this.width = w;
+    }
+  }
+
+  double () {
+    if (this.width && this.height) {
+      this.width *= 2;
+      this.height *= 2;
+    }
+  }
+
+  print () {
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        let row = '';
+        for (let j = 0; j < this.width; j++) {
+          row += 'x';
+        }
+        console.log(row);
+      }
+    }
+  }
+
+  rotate () {
+    if (this.width && this.height) {
+      const temp = this.width;
+      this.width = this.height;
+      this.height = temp;
+    }
+  }
+}
+module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/hom.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+for (let i = 0; i <= 3; i++) {
+  let row = '';
+  for (let j = 0; j <= 4; j++) {
+    
+    row += '*';
+  }
+    console.log(row);  
+}
