2a1031f3f0371221cc93371d5e73694948403935
kamau95 <linuskamau586@gmail.com>
2024-06-05 21:54:51 +0300

    final

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.height = h;
-      this.width = w;
-    }
+    this.width = w;
+    this.height = h;
   }
 }
+
 module.exports = Rectangle;
